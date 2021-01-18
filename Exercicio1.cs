fd27d74ebf911c4b02091e9a461345525b9bd6f7
julio-paes <58228404+julio-paes@users.noreply.github.com>
2021-01-17 23:11:20 -0300

    Add files via upload

---
+++ Exercicio1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace DIO1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] dados = Console.ReadLine().Split();
+            int tempoGasto, velocidadeMedia;
+
+            tempoGasto = int.Parse(dados[0]);
+            velocidadeMedia = int.Parse(dados[1]);
+
+            double calculo = tempoGasto * velocidadeMedia;
+            double litros = calculo / 12;
+
+            Console.WriteLine(string.Format("{0:0.000}", litros));
+        }
+    }
+}
